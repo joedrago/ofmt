24e37463926a60a5778ba5c00b17f61fb07bd0da
Joe Drago <jdrago@netflix.com>
2021-05-07 16:44:55 -0700

    Some basics

--- ofmt/main.py
+++ ofmt/main.py
@@ -1,5 +1,26 @@
+import sys
+import xml.etree.ElementTree as ET
+
+def reformat(xmlFilename):
+    print(f"Reformatting: {xmlFilename}")
+    tree = ET.parse(xmlFilename)
+
+    root = tree.getroot()
+    # Perform sorting on root here
+
+    # tree.write(xmlFilename)
+    tree.write(f"{xmlFilename}.out.xml")
+
 def main():
-    print("Hello World")
+    if len(sys.argv) < 2:
+        print("Syntax: ofmt [xmlFilename] ([xmlFilename]...)")
+        return
+
+    for i, arg in enumerate(sys.argv):
+        if i < 1:
+            continue
+
+        reformat(arg)
 
 if __name__ == "__main__":
     main()
